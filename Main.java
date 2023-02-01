8ade64e4f7756e4fbcdb51a987e0953112a8b313
Sih20 <122289259+Sih20@users.noreply.github.com>
2023-02-01 15:03:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-mkdir -p /tmp/siham/
-rm -rf /tmp/siham/*
-echo ' public class Main { public static void main(String[] args) { System.out.println("Hello, World!"); } } ' >/tmp/siham/Main.java
-javac /tmp/siham/Main.java
+public class Main { 
+    public static void main(String[] args) { 
+        System.out.println("Hello, World!"); 
+    } 
+} 
